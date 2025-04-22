444076e4afa30fa61184c971dc129186b936332c
Mefisto91 <sergiobeltranpatino@gmail.com>
2025-04-21 23:23:39 -0500

    Creación del proyecto y la aplicación en Django

---
+++ MyAppDjango/Models/PrendaModel.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+class PrendaM(models.Model):
+    Talla = models.CharField(max_length=3)
+    color = models.CharField(max_length=20)
+    disponible = models.BooleanField(default=True)
+    cantidad = models.IntegerField
+    
+
---
+++ MyAppDjango/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .Models.PrendaModel import PrendaM
+
+admin.site.register(PrendaM)
+
+
+
+# Register your models here.
